6c4d17629cad5fdf7b8bd2704f1ca21c87661a98
JorgeMarioZV <jorgemariozv@gmail.com>
2025-08-29 00:37:51 -0400

    Calculator

---
+++ interestCalculator/vftvk-Simple-Interest-Calculator-master/script.js
@@ -0,0 +1,10 @@
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
+function compute()
+{
+    p = document.getElementById("principal").value;
+    
+}
